626c24fb72667fc8307686fa3d7a30e96c5b1557
Sprrwkk <sprrwkk@gmail.com>
2019-11-07 21:37:00 +0100

    Create post objects manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+# My managers
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+# Create your models here
 class Post(models.Model):
 
     STATUS_CHOICES = (
@@ -19,9 +26,17 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
+
+    #managers
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
 
