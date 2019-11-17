80ae43c47716effe21ab8e0ec20f9cf211d73471
Sprrwkk <sprrwkk@gmail.com>
2019-11-17 16:45:48 +0100

    Create tag manager

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
+
 
 # My managers
 class PublishedManager(models.Manager):
@@ -32,6 +35,7 @@ class Post(models.Model):
     #managers
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
